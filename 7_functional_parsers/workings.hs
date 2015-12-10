5e3b905332213eebc830af43a41dfc8279e696ba
am2100 <jimnoble@xjjz.co.uk>
2015-12-10 10:37:59 +0000

    Add functional parsers notes & workings

--- 7_functional_parsers/workings.hs
+++ 7_functional_parsers/workings.hs
@@ -1,3 +1,4 @@
+import Prelude hiding (return)
 type Parser a = String -> [(a, String)]
 
 item :: Parser Char
@@ -18,3 +19,11 @@ p +++ q = \inp -> case p inp of
 
 parse :: Parser a -> String -> [(a, String)]
 parse p inp = p inp
+
+char                          :: Char -> Parser Char
+char x                        =  sat (== x)
+
+
+sat                           :: (Char -> Bool) -> Parser Char
+sat p                         =  do x <- item
+                                    if p x then return x else failure
